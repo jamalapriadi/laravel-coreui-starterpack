49ba95d71aadf221010e81124c94226faadaae79
jamalapriadi <jamal.apriadi@gmail.com>
2019-12-14 07:24:31 +0700

    add js

--- resources/js/components/calendars/create.vue
+++ resources/js/components/calendars/create.vue
@@ -1,5 +1,5 @@
 <template>
-    <form @submit.prevent="store" action="/data/calendar" method="post" enctype="multipart/form-data">
+    <form @submit.prevent="store" action="data/calendar" method="post" enctype="multipart/form-data">
         <div class="row">
             <div class="col-lg-12">
                 <div class="card card-flat">
@@ -171,7 +171,7 @@ export default {
             return moment(date).format('h:mm:ss a');
         },
         getCategory(){
-            axios.get('/data/list-category')
+            axios.get('data/list-category')
                 .then(response => {
                     this.categories = response.data;
                 })  
@@ -194,7 +194,7 @@ export default {
         },
 
         showRelatedNews(){
-            axios.get('/data/list-post?type=artikel')
+            axios.get('data/list-post?type=artikel')
                 .then(response => {
                     this.relatednews = response.data;
                     console.log(this.relatednews);
@@ -206,7 +206,7 @@ export default {
                 page = 1;
             }
 
-            axios.get('/data/list-post?type=artikel&q='+this.pencarian)
+            axios.get('data/list-post?type=artikel&q='+this.pencarian)
                 .then(response => {
                     this.relatednews = response.data;
                 })
